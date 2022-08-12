4d3b50c1b9a0c62317e0180256a0ca01ee399a70
haoyunjixiang <1131337572@qq.com>
2022-08-12 23:09:49 +0800

    update

--- code/test.cpp
+++ code/test.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-    cout<<"1111"<<endl;
+    int a[10];
+    int (*p)[10] = &a;
+    for (int i=0;i<10;i++)
+    {
+        a[i] = i*2;
+    }
+
+    cout<<**(p+1)<<endl;
     // https://blog.csdn.net/Strengthennn/article/details/119702982
 }
