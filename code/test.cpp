3029718dc4077048f88793c99772d79c63fecef7
haoyunjixiang <1131337572@qq.com>
2022-08-17 22:31:08 +0800

    update

--- code/test.cpp
+++ code/test.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
+
+void leftuse()
+{
+    int a = 10;
+	int &b = a; // 定义一个左值引用变量
+	b = 20; // 通过左值引用修改引用内存的值
+	cout<<a<<endl;
+}
+
 int main()
 {
-    int a[10];
-    int (*p)[10] = &a;
-    for (int i=0;i<10;i++)
-    {
-        a[i] = i*2;
-    }
-
-    cout<<**(p+1)<<endl;
+    int &a = 10;
+    leftuse();
     // https://blog.csdn.net/Strengthennn/article/details/119702982
 }
